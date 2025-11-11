3b23c5928144ce821d93c3c70bbf7e85f613f0b2
Legandy <legandy@users.noreply.github.com>
2025-11-11 16:55:56 +0100

    App icon added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.material.icons.extended)
     implementation(libs.joor)
     implementation(libs.hiddenapibypass)
+    implementation("androidx.cardview:cardview:1.0.0")
 
 
     // AndroidX & lifecycle
