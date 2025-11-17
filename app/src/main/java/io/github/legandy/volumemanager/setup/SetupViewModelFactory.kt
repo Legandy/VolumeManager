582a4c4c494f5ac70747b4ce808033dafde08c8b
Legandy <legandy@users.noreply.github.com>
2025-11-17 01:45:51 +0100

    Setup migration

---
+++ app/src/main/java/io/github/legandy/volumemanager/setup/SetupViewModelFactory.kt
@@ -0,0 +1,27 @@
+package io.github.legandy.volumemanager.setup
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import io.github.legandy.volumemanager.core.MyApplication
+
+class SetupViewModelFactory : ViewModelProvider.Factory {
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(
+        modelClass: Class<T>,
+        extras: CreationExtras
+    ): T {
+        // Get the Application object from extras
+        val application = checkNotNull(extras[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY])
+        // Create a SavedStateHandle for this ViewModel from extras
+        val savedStateHandle = extras.createSavedStateHandle()
+
+        return SetupViewModel(
+            application,
+            savedStateHandle,
+            MyApplication.setupDataStore
+        ) as T
+    }
+}
