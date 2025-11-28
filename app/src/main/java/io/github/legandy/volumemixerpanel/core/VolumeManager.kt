92b895f5d1c33c8eb23f521c592de4d32dd3c77a
Legandy <legandy@users.noreply.github.com>
2025-11-28 02:38:05 +0100

    Updated Ringer Mode behaviour to change also when DND is on

--- app/src/main/java/io/github/legandy/volumemixerpanel/core/VolumeManager.kt
+++ app/src/main/java/io/github/legandy/volumemixerpanel/core/VolumeManager.kt
@@ -10,8 +10,8 @@ import android.media.AudioPlaybackConfiguration
 import android.provider.Settings
 import android.util.Log
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateMapOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -291,6 +291,6 @@ class VolumeManager(
         val players: MutableList<PlayerEntry> = mutableListOf(),
         val initialVolume: Float = 1f
     ) {
-        var volume by mutableStateOf(initialVolume)
+        var volume by mutableFloatStateOf(initialVolume)
     }
 }
